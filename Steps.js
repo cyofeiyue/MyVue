876f6860f38f48e1a787042991f8008e40a278fe
feiyue <feiyue_zh@163.com>
2017-12-11 21:27:55 +0800

    Initial commit

---
+++ Steps.js
@@ -0,0 +1,14 @@
+new Vue({
+    el:'#app',
+    data:{
+        
+    },
+    mounted:function () {
+        this.$nextTick(function () {
+            
+        })
+    },
+    methods:{
+
+    }
+})
---
+++ readme.md
@@ -0,0 +1,5 @@
+#工作中的一些记录
+#####基于ElementUI做的进度条，
+#####数据使用json模拟，LineViewPanel.json
+#####图标来自阿里巴巴字体图标库
+http://www.iconfont.cn/
